9362d490ddbc937604d8f25266ddb177cddf651f
Niccolò Matteo Borgato <borgato.niccolo@gmail.com>
2023-04-04 17:10:36 +0200

    first draft image slides

--- src/components/componentsBody/Title.tsx
+++ src/components/componentsBody/Title.tsx
@@ -1,5 +1,7 @@
 import "../../App.css";
 import FirstPage from "./componentsTitle/firstPageOptions";
+import ImageShow from "./componentsTitle/imagesShow";
+
 
 // Inputs needed for the navigation bar
 interface Props {
@@ -10,13 +12,13 @@ interface Props {
 }
 
 function TitleDesc({ appTitle, appDescription, firstPage }: Props) {
-  let items = ["Ciao", "Miao", "Bau"];
+  let items = ["Ciao", "Miao"];
   let itemsLink = [
     "https://upload.wikimedia.org/wikipedia/it/thumb/4/4e/Ciao_PX.jpg/1200px-Ciao_PX.jpg",
     "https://www.itagnol.com/wp-content/uploads/2019/01/CIAO.jpg",
   ];
 
-  if (firstPage === "yes") {
+  if (firstPage === "no") {
     return (
       <>
         <div
@@ -40,14 +42,20 @@ function TitleDesc({ appTitle, appDescription, firstPage }: Props) {
     );
   }
   return (
-    <div
-      className="container text-center bg-dark text-light py-5"
-      style={{ height: "100vh", width: "100%" }}
-    >
-      <h1 className="MainTitle">{appTitle}</h1>
-      <h4 className="mt-3">{appDescription}</h4>
-    </div>
-    // ADD IMAGES
+    <>
+      <div
+        className="container text-center bg-dark text-light py-5"
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <h1 className="MainTitle">{appTitle}</h1>
+        <h4 className="mt-3">{appDescription}</h4>
+        <ImageShow
+          imagesTitle={items}
+          imagesDescription={items}
+          imagesLink={itemsLink}
+        />
+      </div>
+    </>
   );
 }
 
